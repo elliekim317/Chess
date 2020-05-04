70a8dda22021a36ef45f86c4b400e84f285ec5b0
elliekim317 <64806772+elliekim317@users.noreply.github.com>
2020-05-04 16:15:11 -0400

    Add files via upload

---
+++ Chess.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Chess extends Application {
+    /**
+     * Main method.
+     * @param args Param for command line
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
